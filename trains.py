f7f103d120dce0c8f30b6fef9b21d10846fd5683
Nathan Lilienthal <nathan@nixpulvis.com>
2025-12-24 23:20:11 -0500

    Remove calculate, it's in parity

--- trains.py
+++ trains.py
@@ -4,24 +4,6 @@ from z3 import *
 DOCK_DURATION = 0.45133333
 CAR_CAPACITY = 32
 
-def calculate_throughput(args):
-    partial = 2 * args.belt * args.cars * (args.rtd - DOCK_DURATION * args.trains) / args.rtd
-    full = CAR_CAPACITY * args.stack * args.trains * args.cars / args.rtd
-    throughput = min(partial, full)
-    print(f"stack size: {args.stack}")
-    print(f"belt speed: {args.belt}")
-    print()
-    print(f"round trip duration: {args.rtd}")
-    print(f"trains: {args.trains}")
-    print(f"cars: {args.cars}")
-    if partial > full:
-        loaded = "full"
-    else:
-        loaded = "partial"
-    print(f"throughput ({loaded}): {throughput}")
-
-
-
 def solve_throughput(args):
     def z3_min(a, b):
         return If(a < b, a, b)
@@ -103,29 +85,13 @@ if __name__ == "__main__":
         description="TODO",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    subparsers = parser.add_subparsers(dest='command', help='Available commands')
-
-    solve_parser = subparsers.add_parser('solve', help='TODO')
-    solve_parser.add_argument("--stack", type=int, default=100, help="Item stack quantity")
-    solve_parser.add_argument("--belt", type=int, default=1200, help="Max belt speed")
-    solve_parser.add_argument("--rtd", type=float, help="Round trip time, otherwise optimized for")
-    solve_parser.add_argument("--trains", type=int, help="Max number of trains")
-    solve_parser.add_argument("--cars", type=int, help="Max number of cars")
-    solve_parser.add_argument("--throughput", type=float, help="Min throughput needed")
-
-    calculate_parser = subparsers.add_parser('calculate', help='TODO')
-    calculate_parser.add_argument("--stack", type=int, default=100, help="Item stack quantity")
-    calculate_parser.add_argument("--belt", type=int, default=1200, help="Belt speed")
-    calculate_parser.add_argument("--rtd", type=float, required=True, help="Round trip time")
-    calculate_parser.add_argument("--trains", type=int, default=1, help="Number of trains")
-    calculate_parser.add_argument("--cars", default=1, type=int, help="Number of cars")
-
-
+    parser.add_argument("--stack", type=int, default=100, help="Item stack quantity")
+    parser.add_argument("--belt", type=int, default=1200, help="Max belt speed")
+    parser.add_argument("--rtd", type=float, help="Round trip time, otherwise optimized for")
+    parser.add_argument("--trains", type=int, help="Max number of trains")
+    parser.add_argument("--cars", type=int, help="Max number of cars")
+    parser.add_argument("--throughput", type=float, help="Min throughput needed")
 
     args = parser.parse_args()
-
-    if args.command == "calculate":
-        calculate_throughput(args)
-    elif args.command == "solve":
-        solve_throughput(args)
+    solve_throughput(args)
 
